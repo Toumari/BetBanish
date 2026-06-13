beb619bbb37d65e1aeb03ed48d369d648b80e0c6
Jack Williams <jack.w141@outlook.com>
2026-06-13 12:06:10 +0100

    feat: expand UK gambling site list to 65 sites

--- sites.js
+++ sites.js
@@ -1,4 +1,5 @@
 const DEFAULT_SITES = [
+  // Traditional UK high-street bookmakers
   'bet365.com',
   'williamhill.com',
   'paddypower.com',
@@ -7,18 +8,75 @@ const DEFAULT_SITES = [
   'coral.co.uk',
   'skybet.com',
   'betvictor.com',
+  'betfred.com',
+  'boylesports.com',
+  'tote.co.uk',
+  'betdaq.co.uk',
+  'smarkets.com',
+  'matchbook.com',
+
+  // Online bookmakers
   'unibet.co.uk',
   'bwin.com',
   '888sport.com',
-  'casumo.com',
   'betway.com',
+  '32red.com',
+  '10bet.co.uk',
+  'betsson.com',
+  'novibet.co.uk',
+  'quinnbet.com',
+  'hollywoodbets.co.uk',
+  'livescorebet.com',
+  'betuk.com',
+  'betgoodwin.co.uk',
+  'midnite.com',
+  'betmgm.co.uk',
+  'mansionbet.com',
+  'sportnation.bet',
+  'vbet.co.uk',
+  'tonybet.co.uk',
+  'bethard.com',
+  'energybet.com',
+
+  // Sky gambling products
+  'skycasino.com',
+  'skyvegas.com',
+  'skypoker.com',
+  'skybingo.com',
+
+  // Online casinos & slots
+  'casumo.com',
   'mrgreen.com',
+  'leovegas.com',
+  '888casino.com',
+  'jackpotjoy.com',
+  'virgingames.com',
+  'gentingcasino.com',
+  'galacasino.com',
+  'mfortune.co.uk',
+  'lottoland.co.uk',
+  'partycasino.com',
   'jackpotcity.com',
-  'draftkings.com',
-  'fanduel.com',
+  'grosvenorcasinos.com',
+  'reeltastic.com',
+
+  // Poker
   'pokerstars.com',
   'partypoker.com',
-  '32red.com'
+  '888poker.com',
+
+  // US operators with UK presence
+  'draftkings.com',
+  'fanduel.com',
+
+  // Bingo
+  'tombola.co.uk',
+  'sunbingo.co.uk',
+  'foxybingo.com',
+  'galabingo.com',
+  '888bingo.com',
+  'meccabingo.com',
+  'winkbingo.com',
 ];
 
 function isGamblingSite(hostname, disabledDefaults, customSites) {
